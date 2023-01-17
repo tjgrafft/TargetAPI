6fa35070a1b02aefa664d206af430342f57576ba
Taylor Grafft <tjgrafft@me.com>
2023-01-16 19:31:00 -0600

    Add files via upload
    Example data that is written in python, but will be stored in Mongo database at later date

---
+++ Target_data.py
@@ -0,0 +1,48 @@
+data = [    
+    {        
+        "id": 1,        
+        "name": "Samsung 55-Inch Smart TV",
+        "price": 499.99,
+        "category": "Electronics",
+        "brand": "Samsung",        
+        "image_url": "https://www.target.com/images/product/12345",
+        "rating": 4.5
+        },    
+    {        
+        "id": 2,
+        "name": "Bose Soundlink Wireless Headphones",
+        "price": 199.99,
+        "category": "Electronics",
+        "brand": "Bose",
+        "image_url": "https://www.target.com/images/product/23456",
+        "rating": 4.2
+        },   
+    {        
+        "id": 3,
+        "name": "Keurig K-Elite Coffee Maker",
+        "price": 129.99,
+        "category": "Kitchen Appliances",
+        "brand": "Keurig",
+        "image_url": "https://www.target.com/images/product/34567",
+        "rating": 4.7
+        },    
+    {        
+        "id": 4,        
+        "name": "KitchenAid Classic Stand Mixer",
+        "price": 299.99,
+        "category": "Kitchen Appliances",
+        "brand": "KitchenAid",
+        "image_url": "https://www.target.com/images/product/45678",
+        "rating": 4.9
+        },
+    {
+        "id": 5,
+        "name": "Apple Airpods Pro",
+        "price": 249.99,
+        "category": "Electronics",
+        "brand": "Apple",
+        "image_url": "https://www.target.com/images/product/56789",
+        "rating": 4.8
+        }
+
+]
